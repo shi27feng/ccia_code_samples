6cb04334e71d855d91a2521eac2f6ba67c343b9f
Feng Shi <shi.feng@cs.ucla.edu>
2022-05-28 10:58:26 +0800

    update files

--- listings/listing_1.1.cpp
+++ listings/listing_1.1.cpp
@@ -3,11 +3,12 @@
 
 void hello()
 {
-    std::cout<<"Hello Concurrent World\n";
+    std::cout << "Hello Concurrent World\n";
 }
 
 int main()
 {
     std::thread t(hello);
+    std::cout << "I'm the main()\n" << std::endl;
     t.join();
 }
--- listings/listing_3.1.cpp
+++ listings/listing_3.1.cpp
@@ -13,14 +13,14 @@ void add_to_list(int new_value)
 bool list_contains(int value_to_find)
 {
     std::lock_guard<std::mutex> guard(some_mutex);
-    return std::find(some_list.begin(),some_list.end(),value_to_find)
-        != some_list.end();
+    return std::find(some_list.begin(),
+                     some_list.end(),
+                     value_to_find) != some_list.end();
 }
 
 #include <iostream>
 
-int main()
-{
+int main() {
     add_to_list(42);
-    std::cout<<"contains(1)="<<list_contains(1)<<", contains(42)="<<list_contains(42)<<std::endl;
+    std::cout << "contains(1) = " << list_contains(1) << ", contains(42) = " << list_contains(42) << std::endl;
 }
